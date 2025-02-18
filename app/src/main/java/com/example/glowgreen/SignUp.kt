db6afabe8eebab21864d4d546528d95471cfecd6
asadlodhi <asadlodhi07@gmail.com>
2025-02-18 15:54:47 +0500

    password masking

--- app/src/main/java/com/example/glowgreen/SignUp.kt
+++ app/src/main/java/com/example/glowgreen/SignUp.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.glowgreen.signup.SignUp
@@ -82,7 +83,8 @@ fun SignUpScreen(modifier: Modifier = Modifier) {
                 value = password,
                 onValueChange = { password = it },
                 label = { Text("Password") },
-                singleLine = true
+                singleLine = true,
+                visualTransformation = PasswordVisualTransformation()
             )
 
         }
